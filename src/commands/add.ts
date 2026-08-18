716496a02b87f272fb42399b4316d0d3acc28306
Rishabh Pal <rishabh8931872407@gmail.com>
2026-08-18 23:21:27 +0530

    task creation domain add

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -1,3 +1,5 @@
+import { type Task } from "../domain/task.js";
+
 export function addCommand(task: string | undefined): void {
   if (!task || task.trim() === "") {
     console.error("Error: Task description cannot be empty.");
@@ -5,5 +7,15 @@ export function addCommand(task: string | undefined): void {
     return;
   }
 
-  console.log(`Task added: ${task}`);
+  const now = new Date().toISOString();
+
+  const newTask: Task = {
+    id: 1,
+    description: task,
+    status: "todo",
+    createdAt: now,
+    updatedAt: now,
+  };
+
+  console.log(newTask);
 }
