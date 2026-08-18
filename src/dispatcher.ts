f11c178e1ad15122603754bdbcca651a7fcb1826
Rishabh Pal <rishabh8931872407@gmail.com>
2026-08-18 22:22:21 +0530

    feat:dispatcher added

---
+++ src/dispatcher.ts
@@ -0,0 +1,62 @@
+export function dispatch(args: string[]): void {
+  const command = args[0];
+
+  function showHelp() {
+    console.log(`
+Task CLI
+
+Usage:
+  task <command>
+
+Commands:
+  add
+  update
+  delete
+  list
+  mark-in-progress
+  mark-done
+
+Options:
+  --help, -h
+`);
+  }
+
+  switch (command) {
+    case "add":
+      console.log("Adding item...");
+      break;
+    case "update":
+      console.log("Updating item...");
+      break;
+    case "delete":
+      console.log("Deleting item...");
+      break;
+    case "list":
+      console.log("Listing items...");
+      break;
+    case "mark-in-progress":
+      console.log("Marking item as in progress...");
+      break;
+    case "mark-done":
+      console.log("Marking item as done...");
+      break;
+
+    case "--help":
+    case "-h":
+      showHelp();
+      break;
+
+    case undefined:
+      console.log(
+        "Command not provided. Use --help or -h for usage information.",
+      );
+      process.exitCode = 1;
+      break;
+
+    default:
+      console.error(
+        `Unknown command: ${command}. \nUse --help or -h for usage information.`,
+      );
+      process.exitCode = 1;
+  }
+}
