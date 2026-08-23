6ae8e9c5d77f3422e177a6c4b3254db06f452b82
Rishabh Pal <rishabh8931872407@gmail.com>
2026-08-24 02:49:30 +0530

    refactor: showHelp command output changed

--- src/dispatcher.ts
+++ src/dispatcher.ts
@@ -8,23 +8,42 @@ import { doneCommand } from "./commands/done.js";
 export async function dispatch(args: string[]): Promise<void> {
   const command = args[0];
 
-  function showHelp() {
+  function showHelp(): void {
     console.log(`
-Task CLI
-
 Usage:
-  task <command>
+  task <command> [options]
 
 Commands:
-  add
-  update
-  delete
-  list
-  mark-in-progress
-  mark-done
 
-Options:
-  --help, -h
+  add <description>
+      Add a new task
+
+  list
+      List all tasks
+
+  update <id> <description>
+      Update a task description
+
+  mark-in-progress <id>
+      Mark a task as in-progress
+
+  done <id>
+      Mark a task as done
+
+  delete <id>
+      Delete a task
+
+  help
+      Show this help message
+
+Examples:
+
+  task add "Learn Node.js"
+  task list
+  task update 1 "Learn Node.js streams"
+  task mark-in-progress 1
+  task done 1
+  task delete 1
 `);
   }
 
