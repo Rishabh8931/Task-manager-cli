1782dd7647d7d993b2be904e049e71f3c57552d5
Rishabh Pal <rishabh8931872407@gmail.com>
2026-08-25 01:56:36 +0530

    refactor: help tui changed

--- src/ui/help.ts
+++ src/ui/help.ts
@@ -1,56 +1,55 @@
 import boxen from "boxen";
-
 import { colors } from "./colors.js";
 import chalk from "chalk";
 
 export function renderHelp(): void {
-  const header = boxen(
+  const content = [
     chalk.yellowBright("A fast developer-focused task manager."),
-    {
+
+    "",
+
+    colors.header("COMMANDS"),
+
+    "",
+
+    command("add <description>", "Add a new task"),
+    command("list", "List all tasks"),
+    command("update <id> <text>", "Update a task"),
+    command("mark-in-progress <id>", "Start a task"),
+    command("done <id>", "Complete a task"),
+    command("delete <id>", "Delete a task"),
+
+    "",
+
+    colors.header("EXAMPLES"),
+
+    "",
+
+    `  ${colors.accent('$ task add "Build REST API"')}`,
+    `  ${colors.accent("$ task list")}`,
+    `  ${colors.accent("$ task mark-in-progress 1")}`,
+    `  ${colors.accent("$ task done 1")}`,
+  ].join("\n");
+
+  console.log(
+    boxen(content, {
       padding: {
         top: 1,
         bottom: 1,
-        left: 1,
-        right: 1,
+        left: 2,
+        right: 2,
       },
+
       title: chalk.bold.redBright("TASK CLI"),
+
       borderStyle: "round",
       borderColor: "greenBright",
+
       titleAlignment: "center",
-    },
+    }),
   );
-
-  console.log(header);
-  console.log();
-
-  console.log(colors.header("COMMANDS"));
-  console.log();
-
-  command("add <description>", "Add a new task");
-  command("list", "List all tasks");
-  command("update <id> <text>", "Update a task");
-  command("mark-in-progress <id>", "Start a task");
-  command("done <id>", "Complete a task");
-  command("delete <id>", "Delete a task");
-
-  console.log();
-
-  console.log(colors.header("EXAMPLES"));
-  console.log();
-
-  console.log(`  ${colors.accent('$ task add "Build REST API"')}`);
-
-  console.log(`  ${colors.accent("$ task list")}`);
-
-  console.log(`  ${colors.accent("$ task mark-in-progress 1")}`);
-
-  console.log(`  ${colors.accent("$ task done 1")}`);
-
-  console.log();
 }
 
-function command(syntax: string, description: string): void {
-  console.log(
-    `  ${colors.primary(syntax.padEnd(25))}` + `${colors.muted(description)}`,
-  );
+function command(syntax: string, description: string): string {
+  return `  ${colors.primary(syntax.padEnd(25))}${colors.muted(description)}`;
 }
--- src/ui/message.ts
+++ src/ui/message.ts
@@ -2,9 +2,11 @@ import { colors } from "./colors.js";
 import { symbols } from "./symbols.js";
 
 export function success(message: string): void {
+  console.log();
   console.log(`${colors.success(symbols.success)} ${message}`);
 }
 
 export function error(message: string): void {
+  console.log();
   console.error(`${colors.error(symbols.error)} ${message}`);
 }
